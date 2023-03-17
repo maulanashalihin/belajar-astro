f72433837867652a4a9a6fbf445b937782e364e1
Maulana Shalihin <maulanashalihin@MacBook-Air.local>
2023-03-17 15:19:23 +0800

    image lazy load

---
+++ src/components/Image.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {onMount} from "svelte" 
+    export let src="https://dripsender.id/wp-content/uploads/2023/02/testi1.jpg"
+    import { scale } from 'svelte/transition';
+    
+    let visible = false;
+    onMount(()=>{
+        visible = true;
+    })
+</script> 
+<div class="invisible text-xs">
+.
+</div>
+{#if visible}
+<img transition:scale={{ delay: 1000, duration: 1000 }} src="{src}" alt="">
+{/if}
