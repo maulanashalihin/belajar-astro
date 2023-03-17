c729a63fb391c31952e8f29e8bd2ee5093b32ca3
Maulana Shalihin <maulanashalihin@MacBook-Air.local>
2023-03-17 11:39:25 +0800

    price calculator

---
+++ src/components/price_calculator.svelte
@@ -0,0 +1,10 @@
+<script>
+    let base_price = 100;
+    let number_users = 1;
+</script>
+<div>
+   <input class="border px-3 py-2 " type="number" bind:value="{number_users}">
+   <div>
+    Harga : {base_price * number_users}
+   </div>
+</div>
